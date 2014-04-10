a412e89a5023d4a7a2b62c8cca330281960ac8b1
gjqt <642941824@qq.com>
2014-04-10 10:48:51 +0800

    Update local_storage_manager.js

--- js/local_storage_manager.js
+++ js/local_storage_manager.js
@@ -19,8 +19,8 @@ window.fakeStorage = {
 };
 
 function LocalStorageManager() {
-  this.bestScoreKey     = "bestScore";
-  this.gameStateKey     = "gameState";
+  this.bestScoreKey     = "bestScore_mr";
+  this.gameStateKey     = "gameState_mr";
 
   var supported = this.localStorageSupported();
   this.storage = supported ? window.localStorage : window.fakeStorage;
