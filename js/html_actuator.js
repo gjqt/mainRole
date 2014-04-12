c668a6a7c0efa16542ac025b836468e919d8b7b6
bjdc <997285765@qq.com>
2014-04-12 09:00:54 +0800

    bug

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -171,7 +171,7 @@ HTMLActuator.prototype.message = function (won) {
   
   var text3 = function (m) { var r = 0; while (m > 1) r++, m >>= 1; return r; }
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "恭喜通关！" : mytxt[text3(maxscore)-3];
+  var message = won ? "恭喜通关！" : mytxt[text3(maxValue)-3];
 
   if (typeof ga !== "undefined") {
     ga("send", "event", "game", "end", type, this.score);
